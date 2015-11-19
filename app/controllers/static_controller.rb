01706866630b7f793afacd9460a64a1eb186d9e9
rrbriggs <ryan.rbriggs@gmail.com>
2015-11-19 08:42:25 +0000

    removed some experiments

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,9 +1,6 @@
 class StaticController < ApplicationController
   before_filter :authenticate_user!
 
-  def dashboard
-  end
-
   def landing
     @qualifications = Qualification.all
     @experiences = Experience.all
